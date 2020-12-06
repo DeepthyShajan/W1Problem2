05f3c7e171b8ea23b1e1b2ba84197a2d11e59247
Deepthy Shajan <Deepthy.Shajan@kpit.com>
2020-12-06 18:07:51 +0530

    Correction in code.

--- MATLABscript/Bumpyroad.m
+++ MATLABscript/Bumpyroad.m
@@ -6,8 +6,8 @@ T1 = 1/f1;
 f2 = 18; 
 T2 = 1/f2; 
 % Make signals.
-signal1 = amp1 * sin(2*pi*f1*t);
-signal2 = amp2 * sin(2*pi*f2*t);
+signal1 = sin(2*pi*f1*t);
+signal2 = sin(2*pi*f2*t);
 signal = signal1 + signal2;
 %plot(t, signal, 'r.-', 'LineWidth', 2, 'MarkerSize', 16);
 t=t';
