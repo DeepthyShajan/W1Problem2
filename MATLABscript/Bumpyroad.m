092d4fe871f5792bb1da9a5158b87135cc8982e2
Deepthy Shajan <Deepthy.Shajan@kpit.com>
2020-12-06 20:20:47 +0530

    Obtained the response of vehicle to road bumps.

--- MATLABscript/Bumpyroad.m
+++ MATLABscript/Bumpyroad.m
@@ -1,14 +1,13 @@
-fs=1000;
-t = 0 : 1/fs : 0.1;
+%t = 0 : 100;
+t=linspace(0,100);
 % sine wave parameters.
-f1 = 10; 
-T1 = 1/f1; 
-f2 = 18; 
-T2 = 1/f2; 
+f1 = 1; 
+f2 = 4;  
 % Make signals.
 signal1 = sin(2*pi*f1*t);
 signal2 = sin(2*pi*f2*t);
 signal = signal1 + signal2;
-%plot(t, signal, 'r.-', 'LineWidth', 2, 'MarkerSize', 16);
+plot(t, signal, 'r.-', 'LineWidth', 2, 'MarkerSize', 16);
 t=t';
 signal=signal';
+sim('SuspensionSystem',100);
